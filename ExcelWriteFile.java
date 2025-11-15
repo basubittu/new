c6614dacb157e226ca531fdda2f7ffd9e3c7dfc1
Abhishek Basu <32103456+basubittu@users.noreply.github.com>
2025-11-16 04:37:11 +0530

    Update ExcelWriteFile.java

--- ExcelWriteFile.java
+++ ExcelWriteFile.java
@@ -6,7 +6,8 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-public class ExcelWriteFile {
+public class ExcelWriteFile 
+{
 
 	public static void main(String[] args) {
 		XSSFWorkbook workbook = new XSSFWorkbook();
